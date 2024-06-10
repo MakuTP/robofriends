7abb403b7d5c1f9c7f7f019489343fa148257873
Maki <mackysuarez19@gmail.com>
2024-06-10 18:22:02 +0800

    Class to Function (useState)

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import Scroll from '../Components/Scroll';
 import ErrorBoundary from '../Components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super ()
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState ([])
+    const [searchfield, setSearchfield] = useState ('')
 
-    componentDidMount(){
+useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(Response=> Response.json())
-            .then(users =>{this.setState({ robots: users})});
-    }
+            .then(users =>{setRobots(users)});
+    },[])
 
-onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+const onSearchChange = (event) => {
+   setSearchfield(event.target.value)
 }
-
-render() {
-    const { robots, searchfield } = this.state;
     const filteredrobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if (robots.length === 0) {
-        return <h1 className='f1 tc'> Loading. . .</h1>
-    }   else {
-        return (
+    return !robots.length ? 
+        <h1 className='f1 tc'> Loading. . .</h1>:
+        (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
                     <ErrorBoundary>
                     <CardList robots={filteredrobots} />
@@ -43,8 +34,6 @@ render() {
                 </Scroll>
             </div>
          );
-        }
-     }  
 }
 
 export default App;
